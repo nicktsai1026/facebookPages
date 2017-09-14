e35ac81cda02191ae8be08922b7f561dcf0de2d2
Nick Tsai <nicktsai1026@gmail.com>
2017-09-14 22:21:17 +0800

    update

--- app/likePages.js
+++ app/likePages.js
@@ -1,28 +1,74 @@
 const axios = require('axios');
-var allData = [];
-var paginationTag = '&limit=100&after=';
+// var allData = [];
+// var paginationTag = '&limit=100&after=';
 
-function getAll(path) {
+
+// function getAll(path) {
+//     return new Promise((resolve, reject) => {
+//         getPages(path);
+//         function getPages(path) {
+//             axios.get(path)
+//                 .then((res) => {
+//                     allData = allData.concat(res.data.data)
+//                     if (res.data.paging) {
+//                         pageAfter = res.data.paging.cursors.after;
+//                         totalPath = path + paginationTag + pageAfter;
+//                         console.log("The next path is " + totalPath);
+//                         getPages(totalPath)
+//                     } else {
+//                         allData.join(',');
+//                         console.log('done!');
+//                         return allData
+//                     }
+//                 })
+//                 .then((data) => {
+//                     if(data != undefined){
+//                         resolve(data)
+//                     }
+//                 })
+//                 .catch((err) => {
+//                     console.log(err);
+//                 })
+//         }
+//     })
+// }
+
+function pageDetails (path) {
     return new Promise((resolve, reject) => {
-        getPages(path);
-        function getPages(path) {
+        var pageData = [];
+        getDetail(path) 
+        function getDetail (path) {
             axios.get(path)
                 .then((res) => {
-                    allData = allData.concat(res.data.data)
-                    if (res.data.paging) {
-                        pageAfter = res.data.paging.cursors.after;
-                        totalPath = path + paginationTag + pageAfter;
-                        console.log("The next path is " + totalPath);
-                        getPages(totalPath)
+                    if (res.data.paging == null) {
+                        pageData.join(',');
+                        console.log('last page is empty');
+                        return pageData;
+                    }
+                    pageData = pageData.concat(res.data.data)
+                    var nextPage = res.data.paging.next;
+                    if (nextPage == null) {
+                        pageData.join(',');
+                        console.log('done');
+                        return pageData
                     } else {
-                        allData.join(',');
-                        console.log('done!');
-                        return allData
+                        console.log(nextPage);
+                        getDetail(nextPage)
                     }
                 })
                 .then((data) => {
-                    if(data != undefined){
-                        resolve(data)
+                    if (data != undefined) {
+                        //......
+                        var pageIdArr = [];
+                        data.forEach((val) => {
+                            pageIdArr.push(val.id);
+                        })
+                        var doubleArr = [];
+                        doubleArr.push(data,pageIdArr);
+                        resolve(doubleArr)
+                        //......
+
+                        //resolve(data)
                     }
                 })
                 .catch((err) => {
@@ -32,4 +78,6 @@ function getAll(path) {
     })
 }
 
-module.exports.getAll = getAll;
+
+module.exports.pageDetails = pageDetails;
+// module.exports.getAll = getAll;
