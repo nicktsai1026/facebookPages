ce49e8645247a7ff65a5b4a716cf1278edcf2fff
Nick Tsai <nicktsai1026@gmail.com>
2017-09-16 14:12:59 +0800

    add passport isLoggedIn

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -16,63 +16,62 @@ module.exports = function (app, db) {
             var path = `https://graph.facebook.com/v2.10/${this.facebookId}/likes?fields=name,fan_count,category,about,link,picture&access_token=${this.accessToken}`;
             getLikePages.pageDetails(path)
                 .then((details) => {
-                    res.redirect('/update');
-                //     var newPageIdOnly = details[1];
-                //     db.collection('users').findOne({ fbId: userId }, (err, item) => {
-                //         if(item.likes) {
-                //             var oldPageIdOnly = [];
-                //             item.likes.forEach((val) => {
-                //                 oldPageIdOnly.push(val.id);
-                //             })
-                //             let a = new Set(oldPageIdOnly);
-                //             let b = new Set(newPageIdOnly);
-                //             // ab difference set差集
-                //             let differenceABSet = new Set([...a].filter(x => !b.has(x)));
-                //             let differenceArr = Array.from(differenceABSet);
+                    var newPageIdOnly = details[1];
+                    db.collection('users').findOne({ fbId: userId }, (err, item) => {
+                        if(item.likes) {
+                            var oldPageIdOnly = [];
+                            item.likes.forEach((val) => {
+                                oldPageIdOnly.push(val.id);
+                            })
+                            let a = new Set(oldPageIdOnly);
+                            let b = new Set(newPageIdOnly);
+                            // ab difference set差集
+                            let differenceABSet = new Set([...a].filter(x => !b.has(x)));
+                            let differenceArr = Array.from(differenceABSet);
 
-                //             differenceArr.forEach((value) => {
-                //                 db.collection('pagedetails').findOne({ id: value }, (err, item) => {
-                //                     var newfbUserId = item.fbUserId.filter(function (id) {
-                //                         return id != userId;
-                //                     })
-                //                     db.collection('pagedetails').updateOne({ id: value }, { $set: { fbUserId: newfbUserId } }, (err, item) => {
-                //                         if (err) return console.log(err)
-                //                     })
-                //                 })
-                //             })
-                //         }
-                //         var pageObj = {};
-                //         pageObj.likes = details[0];
-                //         db.collection('users').updateOne({ fbId: userId }, { $set: pageObj }, (err, item) => {
-                //             if (err) return console.log(err)
-                //         })
-                //     })
-                //     return details[0];
+                            differenceArr.forEach((value) => {
+                                db.collection('pagedetails').findOne({ id: value }, (err, item) => {
+                                    var newfbUserId = item.fbUserId.filter(function (id) {
+                                        return id != userId;
+                                    })
+                                    db.collection('pagedetails').updateOne({ id: value }, { $set: { fbUserId: newfbUserId } }, (err, item) => {
+                                        if (err) return console.log(err)
+                                    })
+                                })
+                            })
+                        }
+                        var pageObj = {};
+                        pageObj.likes = details[0];
+                        db.collection('users').updateOne({ fbId: userId }, { $set: pageObj }, (err, item) => {
+                            if (err) return console.log(err)
+                        })
+                    })
+                    return details[0];
                 })
-                // .then((detailArr) => {
-                //     detailArr.forEach((val) => {
-                //         db.collection('pagedetails').findOne({ id: val.id }, (err, item) => {
-                //             if (item == null) {
-                //                 val.fbUserId = [this.facebookId];
-                //                 db.collection('pagedetails').insert(val, (err, item) => {
-                //                     if (err) return console.log(err)
-                //                 })
-                //             } else {
-                //                 //check fbUserId arr had this facebookId or not
-                //                 var judge = item.fbUserId.indexOf(this.facebookId);
-                //                 if( judge < 0) {
-                //                     item.fbUserId.push(this.facebookId);
-                //                     db.collection('pagedetails').updateOne({ id: val.id}, { $set: item}, (err, item) => {
-                //                         if (err) return console.log(err)
-                //                     })
-                //                 }
-                //             }
-                //         })
-                //     })
-                //     //home should be change
-                //     // res.render('home');
-                //     res.send(pageObj)
-                // })
+                .then((detailArr) => {
+                    detailArr.forEach((val) => {
+                        db.collection('pagedetails').findOne({ id: val.id }, (err, item) => {
+                            if (item == null) {
+                                val.fbUserId = [this.facebookId];
+                                db.collection('pagedetails').insert(val, (err, item) => {
+                                    if (err) return console.log(err)
+                                })
+                            } else {
+                                //check fbUserId arr had this facebookId or not
+                                var judge = item.fbUserId.indexOf(this.facebookId);
+                                if( judge < 0) {
+                                    item.fbUserId.push(this.facebookId);
+                                    db.collection('pagedetails').updateOne({ id: val.id}, { $set: item}, (err, item) => {
+                                        if (err) return console.log(err)
+                                    })
+                                }
+                            }
+                        })
+                    })
+                    //home should be change
+                    res.render('home');
+                    // res.send(pageObj)
+                })
                 .catch((err) => {
                     console.log(err);
                 })
